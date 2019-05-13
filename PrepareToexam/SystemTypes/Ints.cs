efa88e7163ab9bfd186b8df5c2978e1b60e0a749
tirans3 <tirans3@mail.ru>
2019-05-13 20:57:58 +0400

    Add int type inform

--- PrepareToexam/SystemTypes/Ints.cs
+++ PrepareToexam/SystemTypes/Ints.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SystemTypes
+﻿namespace SystemTypes
 {
     class Ints
     {
+        // Inheritance Object -> ValueType ->Int32.
+        // Aliase in C# is int.
+        // Size Signed 32-bit integer.
+        // Approximate Range from -2,147,483,648 to 2,147,483,647.
+        // Default value is 0.
+        int intValue1 = 90_946;                               // 90946
+        int intValue2 = 0x0001_6342;                          // 90946 
+        int intValue3 = 0b0001_0110_0011_0100_0010;           // 90946
+                                                              // int intValue4 = 0x_0001_6342;                       // C# 7.2 onwards  90946
+                                                              // int intValue5 = 0b_0001_0110_0011_0100_0010;        // C# 7.2 onwards  90946
+
     }
 }
