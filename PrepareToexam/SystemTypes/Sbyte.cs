d0e2f2578e1ee4cbec22eb856ce4a9b72cb0e635
tirans3 <tirans3@mail.ru>
2019-05-13 16:52:50 +0400

    Add Sbyte type inform

--- PrepareToexam/SystemTypes/Sbyte.cs
+++ PrepareToexam/SystemTypes/Sbyte.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SystemTypes
+﻿namespace SystemTypes
 {
     class Sbyte
     {
+        // Inheritance Object -> ValueType -> SByte.
+        // Aliase in C# is sbyte.
+        // Range from -128 to 127.
+        // Size is 1 byte.
+        // Default value is 0.
+        readonly sbyte Default = 0;
+        readonly sbyte min = sbyte.MinValue;// -128;
+        readonly sbyte max = sbyte.MaxValue;// 127;
     }
 }
