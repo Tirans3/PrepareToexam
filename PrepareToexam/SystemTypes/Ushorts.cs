f12b644828684f36af4e701f0a30b9abea4379d7
tirans3 <tirans3@mail.ru>
2019-05-13 21:45:40 +0400

    Add ushort type inform

---
+++ PrepareToexam/SystemTypes/Ushorts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemTypes
+{
+    class Ushorts
+    {
+        // Inheritance Object -> ValueType ->UInt16.
+        // Aliase in C# is ushort.
+        // Size Unsigned 16-bit integer.
+        // Range from 0 to 65,535.
+        // Default value is 0. 
+    }
+}
