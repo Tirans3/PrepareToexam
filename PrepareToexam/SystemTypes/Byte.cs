5003a300c9aee968f9fccda01dda5bb39cf4e36d
tirans3 <tirans3@mail.ru>
2019-05-13 16:37:53 +0400

    Add Byte type inform

--- PrepareToexam/SystemTypes/Byte.cs
+++ PrepareToexam/SystemTypes/Byte.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SystemTypes
+﻿namespace SystemTypes
 {
     class Byte
     {
+        // Inheritance Object -> ValueType -> Byte.
+        // Aliase in c# is byte.
+        // Size is 1 byte.
+        // Range from 0 to 255.
+        // Default valu is 0.
+        readonly byte Default = 0;
+        readonly byte min = byte.MinValue;// 0;
+        readonly byte max = byte.MaxValue;// 255;
+
     }
 }
