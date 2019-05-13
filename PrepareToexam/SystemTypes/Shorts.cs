cd8ecd10b68fe3feea95eb88252da7a5e31e3952
tirans3 <tirans3@mail.ru>
2019-05-13 21:41:34 +0400

    Add short type inform

--- PrepareToexam/SystemTypes/Shorts.cs
+++ PrepareToexam/SystemTypes/Shorts.cs
@@ -1,12 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace SystemTypes
 {
     class Shorts
     {
+        // Inheritance Object -> ValueType ->Int16.
+        // Aliase in C# is short.
+        // Size Signed 16-bit integer.
+        // Range from -32,768 to 32,767.
+        // Default value is 0.  
+
+        // short x = 3.0;           Error: no implicit conversion from double
+           short y = (short)3.0;   // OK: explicit conversion
     }
 }
