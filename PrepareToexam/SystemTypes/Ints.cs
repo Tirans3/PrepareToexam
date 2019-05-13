c8482a11d6b67b9189c38835bab86c93a36367a4
tirans3 <tirans3@mail.ru>
2019-05-13 21:11:08 +0400

    Add int type inform

--- PrepareToexam/SystemTypes/Ints.cs
+++ PrepareToexam/SystemTypes/Ints.cs
@@ -2,7 +2,7 @@
 {
     class Ints
     {
-        // Inheritance Object -> ValueType ->Int32.
+        // Inheritance Object -> ValueType -> Int32.
         // Aliase in C# is int.
         // Size Signed 32-bit integer.
         // Approximate Range from -2,147,483,648 to 2,147,483,647.
@@ -10,8 +10,8 @@
         int intValue1 = 90_946;                               // 90946
         int intValue2 = 0x0001_6342;                          // 90946 
         int intValue3 = 0b0001_0110_0011_0100_0010;           // 90946
-                                                              // int intValue4 = 0x_0001_6342;                       // C# 7.2 onwards  90946
-                                                              // int intValue5 = 0b_0001_0110_0011_0100_0010;        // C# 7.2 onwards  90946
+     // int intValue4 = 0x_0001_6342;                            90946    C# 7.2 onwards  
+     // int intValue5 = 0b_0001_0110_0011_0100_0010;             90946    C# 7.2 onwards 
 
     }
 }
--- PrepareToexam/SystemTypes/Units.cs
+++ PrepareToexam/SystemTypes/Units.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SystemTypes
+﻿namespace SystemTypes
 {
     class Units
     {
+        // Inheritance Object -> ValueType ->UInt32.
+        // Aliase in C# is uint.
+        // Size Unsigned 32-bit integer.
+        // Approximate Range from0 to 4,294,967,295.
+        // Default value is 0.                                                  
+        uint uintValue1 = 3_000_000_000;                                       //3000000000
+        uint uintValue2 = 0xB2D0_5E00;                                         //3000000000
+        uint uintValue3 = 0b1011_0010_1101_0000_0101_1110_0000_0000;           //3000000000
+      //uint uintValue4 = 0x_B2D0_5E00;                                          3000000000   C# 7.2 onwards
+      //uint uintValue5 = 0b_1011_0010_1101_0000_0101_1110_0000_0000;            3000000000   C# 7.2 onwards
     }
 }
