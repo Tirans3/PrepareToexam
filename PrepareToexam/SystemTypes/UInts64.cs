9659542c01e3dc8ed2ca58ccf88e2d611487bf94
tirans3 <tirans3@mail.ru>
2019-05-13 21:35:01 +0400

    Add type inform

--- PrepareToexam/SystemTypes/UInts64.cs
+++ PrepareToexam/SystemTypes/UInts64.cs
@@ -8,5 +8,17 @@ namespace SystemTypes
 {
     class UInts64
     {
+        // Inheritance Object -> ValueType ->UInt64.
+        // Aliase in C# is ulong.
+        // Size Unsigned 64-bit integer.
+        // Range from 0 to 18,446,744,073,709,551,615
+        // Default value is 0. 
+
+         /* Error -- no implicit conversion from double:
+            ulong x = 3.0;
+         */
+        // OK -- explicit conversion:
+         ulong y = (ulong)3.0;
+
     }
 }
