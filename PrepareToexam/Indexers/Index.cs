393c12778eb9d6c38caccecc7046b6c1bc70a7f4
tirans3 <tirans3@mail.ru>
2019-05-20 14:59:41 +0400

    Add indexer

--- PrepareToexam/Indexers/Index.cs
+++ PrepareToexam/Indexers/Index.cs
@@ -1,10 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Indexers
+﻿namespace Indexers
 {
     class Index
     {
+        public int? X { get; set; }
+        public string Y { get; set; }
+
+        public int? this[string i]
+        {
+            get
+            {
+                return i == Y ? X : null;
+            }
+
+            set
+            {
+                X = i == Y ? value : null;
+            }
+        }
     }
 }
